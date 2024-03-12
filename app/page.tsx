6919c7186661ef627aa6358fb2cdf8069e641bd5
Murial Anindo <murial.anindo@lonett.com>
2024-03-11 20:32:06 -0400

    new portfolio app

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+  <div>
+  <Head>
+    <title>Murials Porfolio</title>
+  </Head>
+  <main className="flex min-h-screen flex-col items-center justify-between p-24">
+ <section>
+  
+ </section>
+
+  </main>
+  </div>
+  );
+}
